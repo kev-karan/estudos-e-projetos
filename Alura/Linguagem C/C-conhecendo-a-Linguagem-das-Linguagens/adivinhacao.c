b7b1067948e71be3496bd1abe84608bd079d7984
kev-karan <kevkaran@proton.me>
2026-03-28 19:18:58 -0300

    Atualiza lógica do jogo de adivinhação para gerar número secreto aleatório e remove valor fixo

--- Alura/Linguagem C/C-conhecendo-a-Linguagem-das-Linguagens/adivinhacao.c
+++ Alura/Linguagem C/C-conhecendo-a-Linguagem-das-Linguagens/adivinhacao.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main()
 {
@@ -7,11 +8,14 @@ int main()
     printf("* Bem vindo ao nosso jogo de adivinhação *\n");
     printf("******************************************\n");
 
-    int numeroSecreto = 42;
+    int segundos = time(0);
+    srand(segundos);
 
+    int numeroGrande = rand();
+
+    int numeroSecreto = numeroGrande % 100;
     int chute;
     int tentativas = 1;
-
     float pontos = 1000;
 
     while (1)
