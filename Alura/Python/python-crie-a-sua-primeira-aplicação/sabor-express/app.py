a98adf437659d842e2eff8b5406459c518870ce5
kev-karan <kevkaran@proton.me>
2026-04-02 05:50:17 -0300

    Refactor menu display and option selection logic in the Sabor Express app

--- Alura/Python/python-crie-a-sua-primeira-aplicação/sabor-express/app.py
+++ Alura/Python/python-crie-a-sua-primeira-aplicação/sabor-express/app.py
@@ -1,13 +1,21 @@
 import os
 
-print("Sabor Express\n")
 
-print("1. Cadastrar restaurante")
-print("2. Listar restaurante")
-print("3. Ativar restaurante")
-print("4. Sair")
+def exibir_nome_do_programa():
+    print("Sabor Express\n")
 
-opcao_escolhida = int(input("Escolha uma opção: "))
+
+def exibir_opcoes():
+    print("1. Cadastrar restaurante")
+    print("2. Listar restaurante")
+    print("3. Ativar restaurante")
+    print("4. Sair")
+
+
+def opcao_invalida():
+    print("Opção inválida!\n")
+    input("Digite uma tecla para voltar ao menu principal")
+    main()
 
 
 def finalizar_app():
@@ -15,11 +23,31 @@ def finalizar_app():
     print("Encerrando o programa\n")
 
 
-if opcao_escolhida == 1:
-    print("Cadastrar restaurante")
-elif opcao_escolhida == 2:
-    print("Listar restaurantes")
-elif opcao_escolhida == 3:
-    print("Ativar restaurante")
-else:
-    finalizar_app()
+def escolher_opcoes():
+    try:
+        opcao_escolhida = int(input("Escolha uma opção: "))
+
+        match opcao_escolhida:
+            case 1:
+                print("Cadastrar restaurante")
+            case 2:
+                print("Listar restaurantes")
+            case 3:
+                print("Ativar restaurante")
+            case 4:
+                finalizar_app()
+            case _:
+                opcao_invalida()
+    except Exception:
+        opcao_invalida()
+
+
+def main():
+    os.system("cls")
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcoes()
+
+
+if __name__ == "__main__":
+    main()
