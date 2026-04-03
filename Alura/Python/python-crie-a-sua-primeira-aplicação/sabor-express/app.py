1631256578eaa3cc8080bd41975d9159ea0ef2ee
kev-karan <kevkaran@proton.me>
2026-04-03 05:26:10 -0300

    Refactor restaurant data structure and improve listing format in Sabor Express app

--- Alura/Python/python-crie-a-sua-primeira-aplicação/sabor-express/app.py
+++ Alura/Python/python-crie-a-sua-primeira-aplicação/sabor-express/app.py
@@ -1,6 +1,10 @@
 import subprocess
 
-restaurantes = ["Pizza", "Sushi"]
+restaurantes = [
+    {"nome": "Praça", "categoria": "Japonesa", "ativo": False},
+    {"nome": "Pizza Superma", "categoria": "Pizza", "ativo": True},
+    {"nome": "Cantina", "categoria": "Italiano", "ativo": False},
+]
 
 
 def exibir_nome_do_programa():
@@ -45,7 +49,10 @@ def cadastrar_novo_restaurante():
 def listar_restaurantes():
     exibir_subtitulo("Listando restaurantes")
     for restaurante in restaurantes:
-        print(f".{restaurante}")
+        nome_restaurante = restaurante["nome"]
+        categoria = restaurante["categoria"]
+        ativo = restaurante["ativo"]
+        print(f" - {nome_restaurante} | {categoria} | {ativo}")
     voltar_ao_menu_principal()
 
 
