e1cfcf7ce86d99ddd6f3cd594a31c9b61f0f5303
kev-karan <kevkaran@proton.me>
2026-04-05 03:59:32 -0300

    Refactor forca.c: improve code structure and readability

--- Alura/Linguagem C/C-avançando-na-linguagem/forca.c
+++ Alura/Linguagem C/C-avançando-na-linguagem/forca.c
@@ -1,6 +1,57 @@
 #include <stdio.h>
+#include <string.h>
+
+void abertura()
+{
+    printf("*************************\n");
+    printf("*     Jogo de Forca     *\n");
+    printf("*************************\n\n");
+}
+
+void chuta(char chutes[26], int* tentativas)
+{
+    char chute;
+    scanf(" %c", &chute);
+
+    chutes[(*tentativas)] = chute;
+    (*tentativas)++;
+}
 
 int main()
 {
     char palavraSecreta[20];
+
+    sprintf(palavraSecreta, "MELANCIA");
+
+    int acertou = 0;
+    int enforcou = 0;
+
+    char chutes[26];
+    int tentativas = 0;
+
+    abertura();
+
+    do {
+        for (int i = 0; i < strlen(palavraSecreta); i++) {
+
+            int achou = 0;
+
+            for (int j = 0; j < tentativas; j++) {
+                if (chutes[j] == palavraSecreta[i]) {
+                    achou = 1;
+                    break;
+                }
+            }
+
+            if (achou) {
+                printf("%c ", palavraSecreta[i]);
+            } else {
+                printf("_ ");
+            }
+        }
+        printf("\n");
+
+        chuta(chutes, &tentativas);
+
+    } while (!acertou && !enforcou);
 }
