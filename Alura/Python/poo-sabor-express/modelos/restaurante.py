d5b4eb83abaa743a5f170fc18f3f784d77edbc26
kev-karan <kevkaran@proton.me>
2026-04-12 05:39:40 -0300

    Fix category attribute typo and enhance Restaurante class with listing functionality

--- Alura/Python/poo-sabor-express/modelos/restaurante.py
+++ Alura/Python/poo-sabor-express/modelos/restaurante.py
@@ -1,8 +1,21 @@
 class Restaurante:
+    restaurantes = []
+
     def __init__(self, nome, categoria):
         self.nome = nome
-        self.cadegoria = categoria
+        self.categoria = categoria
         self.ativo = False
+        Restaurante.restaurantes.append(self)
+
+    def __str__(self):
+        return f"{self.nome} | {self.categoria}"
+
+    def listar_restaurantes():
+        for restarante in Restaurante.restaurantes:
+            print(f"{restarante.nome} | {restarante.categoria} | {restarante.ativo}")
 
 
 restaurante_praca = Restaurante("Praça", "Gourmet")
+restaurante_pizza = Restaurante("Pizza Express", "Italiana")
+
+Restaurante.listar_restaurantes()
