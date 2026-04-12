e5531bd173db722fa626114ac4e7fb3d8f87a66b
kev-karan <kevkaran@proton.me>
2026-04-12 20:56:58 -0300

    Add Avaliacao class and implement receiving evaluations in Restaurante class

--- Alura/Python/poo-sabor-express/app.py
+++ Alura/Python/poo-sabor-express/app.py
@@ -5,6 +5,10 @@ restaurante_mexicano = Restaurante("Mexican Food", "mexicana")
 restaurante_japones = Restaurante("japa", "japonesa")
 restaurante_mexicano.alternar_estado()
 
+restaurante_praca.receber_avaliacao("Gui", 10)
+restaurante_praca.receber_avaliacao("Lais", 8)
+restaurante_praca.receber_avaliacao("Emy", 5)
+
 
 def main():
     Restaurante.listar_restaurantes()
