988a00a528ed06b97fbcdb7d82cebad3e2b5ed23
kev-karan <kevkaran@proton.me>
2026-04-12 03:45:57 -0300

    Add Restaurante class with initialization and instance creation

---
+++ Alura/Python/poo-sabor-express/modelos/restaurante.py
@@ -0,0 +1,8 @@
+class Restaurante:
+    def __init__(self, nome, categoria):
+        self.nome = nome
+        self.cadegoria = categoria
+        self.ativo = False
+
+
+restaurante_praca = Restaurante("Praça", "Gourmet")
